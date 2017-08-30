f95d73bbbd034f99bd3feb4994df7ac3e86b2ca4
tjohns0n <tjohns0n@rams.colostate.edu>
2017-08-29 21:04:37 -0600

    Add base for sprint 1

--- src/test/java/edu/csu2017fa314/DTR02/AllTests.java
+++ src/test/java/edu/csu2017fa314/DTR02/AllTests.java
@@ -1,6 +1,6 @@
 package edu.csu2017fa314.DTR02;
-import edu.csu2017fa314.DTR01.Model.*;
-import edu.csu2017fa314.DTR01.View.*;
+import edu.csu2017fa314.DTR02.Model.*;
+import edu.csu2017fa314.DTR02.View.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import junit.framework.JUnit4TestAdapter;
