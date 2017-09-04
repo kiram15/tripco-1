0e6a9b744a42c3e305aa5a34b4221ddfa2a1ce6c
alnolte <31218518+alnolte@users.noreply.github.com>
2017-09-03 22:29:50 -0500

    Create Distance.java

--- src/main/java/edu/csu2017fa314/T02/Model/Distance.java
+++ src/main/java/edu/csu2017fa314/T02/Model/Distance.java
@@ -1,5 +1,26 @@
 package edu.csu2017fa314.T02.Model;
-public class Distance
-{
+public class Distance{
+  
+  private String startID;
+  private String endID;
+  private int gcd;
+
+  public Distance(String startID, String endID, int gcd){
+      this.startID = startID;
+      this.endID = endID;
+      this.gcd = gcd;
+  }
+
+  public String getStartID(){ 
+      return this.startID; 
+  }
+  
+  public String getEndID(){ 
+      return this.endID; 
+  }
+  
+  public int getGcd(){ 
+      return this.gcd; 
+  }
 
 }
