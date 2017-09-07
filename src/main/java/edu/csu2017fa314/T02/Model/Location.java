1f89ab6f90bef23dde15dfa48263d5b8c652c3ff
Kira Miller <31229189+kiram15@users.noreply.github.com>
2017-09-06 19:15:05 -0600

    Update Location.java

--- src/main/java/edu/csu2017fa314/T02/Model/Location.java
+++ src/main/java/edu/csu2017fa314/T02/Model/Location.java
@@ -1,5 +1,23 @@
 package edu.csu2017fa314.T02.Model;
-public class Location
-{
+public class Location {
+    private String ID;
+    private String name;
+    private String city;
+    private String lat;
+    private String lon;
+    private int    altitude;
+
+  public Location(String id, String Name, String City, String Lat, String Lon, int Elevation) {
+      ID = id;
+      name = Name;
+      city = City;
+      lat = Lat;
+      lon = Lon;
+      altitude = Elevation;
+  }
   
+  public String toString() { 
+	    return "ID: '" + this.ID + "', Name: '" + this.name + "', City: '" + this.city + "'" +
+	    		"Lat: '" + this.lat + "', Lon: '" + this.lon + "', Altitude: '" + this.altitude + "'";
+	} 
 }
