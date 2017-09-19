d2d06a4596d4f10d6eb20678c99dbe036cb4a329
Emerson McIntosh <emerson.mcintosh@gmail.com>
2017-09-19 16:33:43 -0600

    Added GCD tests

--- src/test/java/edu/csu2017fa314/T02/Model/TestModel.java
+++ src/test/java/edu/csu2017fa314/T02/Model/TestModel.java
@@ -9,11 +9,13 @@ import java.io.File;
 public class TestModel
 {
     private Model m;
+    private Hub h;
 
     @Before
     public void setUp() throws Exception
     {
         m = new Model();
+        h = new Hub();
     }
 
     @Test
@@ -22,6 +24,30 @@ public class TestModel
         assertArrayEquals(m.getNumbers(), new int[] {0, 1, 2, 3, 4, 5});
     }
 
-    
+    @Test
+    public void testDiagonalGCD(){
+        assertEquals(466, h.greatCirDist(37, -102, 41, -109));
+        assertEquals(466, h.greatCirDist(41, -109, 37, -102));
+    }
+
+    @Test
+    public void testSameLonGCD(){
+        assertEquals(276, h.greatCirDist(37, -102, 41, -102));
+    }
+
+    @Test
+    public void testSameLatGCD(){
+        assertEquals(386, h.greatCirDist(37, -102, 37, -109));
+    }
+
+    @Test
+    public void testSameLocationGCD(){
+        assertEquals(0, h.greatCirDist(37, -102, 37, -102));
+    }
+
+    @Test
+    public void testReverseGCD(){
+        assertEquals(h.greatCirDist(37, -102, 41, -109), h.greatCirDist(41, -109, 37, -102));
+    }
 
 }
