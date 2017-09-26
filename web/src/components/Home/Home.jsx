f04c56216064c3a138304f311aa1d795cdb5e872
Amber Nolte <alnolte@salem.cs.colostate.edu>
2017-09-26 15:12:19 -0600

    adding lines

--- web/src/components/Home/Home.jsx
+++ web/src/components/Home/Home.jsx
@@ -27,6 +27,10 @@ class Home extends React.Component {
 
 		<div id = "svgCOrectangle">
 		  <svg xmlns="http://www.w3.org/2000/svg" width="640" height="480">
+ 		    <g>
+  		      <title>Layer 1</title>
+  		      <rect stroke="#000000" id="svg_1" height="138" width="193" y="250" x="350" stroke-width="5" fill="#ffffff"/>
+		    </g>
 		  </svg> 
 		</div>
 
