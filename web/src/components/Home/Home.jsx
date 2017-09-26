2f3384a3d078aba2f28d31469c7a0d653ff8ebc3
Amber Nolte <alnolte@salem.cs.colostate.edu>
2017-09-26 14:50:06 -0600

    remove comment

--- web/src/components/Home/Home.jsx
+++ web/src/components/Home/Home.jsx
@@ -27,7 +27,6 @@ class Home extends React.Component {
 
 		<div>
 		<svg width="640" height="480" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
- 		<!-- Created with SVG-edit - http://svg-edit.googlecode.com/ -->
  		<g>
   		<title>Layer 1</title>
   		<rect stroke="#000000" id="svg_1" height="138" width="193" y="250" x="350" stroke-width="5" fill="#ffffff"/>
