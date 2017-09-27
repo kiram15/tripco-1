c07a1d6315264c7c8d6694d529dd3b3965f1a5ff
alnolte <alnolte@rams.colostate.edu>
2017-09-26 22:20:25 -0600

    set up io streams

--- web/src/components/Home/drawSVG.java
+++ web/src/components/Home/drawSVG.java
@@ -1,1 +1,18 @@
+import jdk.internal.util.xml.impl.Input;
 
+import java.io.*;
+
+public class drawSVG{
+    FileInputStream fStream = new FileInputStream("COmap.svg");
+    String nextLine;
+    nextLine = fStream;
+    while(nextLine != null){
+        
+    }
+
+    File file = new File("CoMapTripCo.svg");
+    file.getParentFile().mkdirs();
+    PrintWriter pw = new PrintWriter(file);
+    pw.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>");
+
+}
