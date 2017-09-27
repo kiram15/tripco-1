d2c481ac9963aa4d1c14d27c8a39f954a1046c28
Amber Nolte <alnolte@denver.cs.colostate.edu>
2017-09-26 20:26:56 -0600

    adding rectangle to website

--- web/src/components/Home/Home.jsx
+++ web/src/components/Home/Home.jsx
@@ -26,14 +26,11 @@ class Home extends React.Component {
                 </div>
 
 		<div id = "svgCOrectangle">
-		  <svg width="640" height="480"
-			xmlns="http://www.w3.org/2000/svg"
-			xmlns:xlink="http://www.w3.org/1999/xlink">
+		  <svg xmlns="http://www.w3.org/2000/svg" width="640" height="480">
  		    <g>
   		      <title>Layer 1</title>
   		      <rect stroke="#000000" id="svg_1" height="138" width="193" y="250" x="350" stroke-width="5" fill="#ffffff"/>
 		    </g>
-		    <image x="350" y="250" width="1066.6073" height="783.0824" xlink:href="COmap.svg"/>
 		  </svg> 
 		</div>
 
