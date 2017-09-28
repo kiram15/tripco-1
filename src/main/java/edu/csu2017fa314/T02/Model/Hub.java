f700f76ff210cbb2af9b91c888b430fa0dced75d
alnolte <alnolte@rams.colostate.edu>
2017-09-27 19:51:39 -0600

    add types

--- src/main/java/edu/csu2017fa314/T02/Model/Hub.java
+++ src/main/java/edu/csu2017fa314/T02/Model/Hub.java
@@ -356,35 +356,35 @@ public class Hub {
         double originStartLon = 0.0;
         double finalEndLat = 0.0;
         double finalEndLon = 0.0;
-        bool first = false;
+        boolean first = false;
         for(Distance d : shortestItinerary){
-            unitHeight = 195.7706; //COmap height/4
-            unitWidth = 152.3724714; //COmap width/7
+            double unitHeight = 195.7706; //COmap height/4
+            double unitWidth = 152.3724714; //COmap width/7
             if(!first){
                 originStartLat = d.getStartID().getLatitude();
                 originStartLon = d.getStartID().getLongitude();
                 first = true;
             }
-            startLat = d.getStartID().getLatitude();
-            startLon = d.getStartID().getLongitude();
-            endLat = d.getEndID().getLatitude();
-            endLon = d.getEndID().getLongitude();
+            double startLat = d.getStartID().getLatitude();
+            double startLon = d.getStartID().getLongitude();
+            double endLat = d.getEndID().getLatitude();
+            double endLon = d.getEndID().getLongitude();
 
             finalEndLat = d.getEndID().getLatitude();
             finalEndLon = d.getEndID().getLongitude();
 
-            x1 = (startLon + 109) * unitHeight) + 38;
-            y1 = (startLat - 41) * unitWidth) + 38;
-            x2 = (endLon + 109) * unitHeight) + 38;
-            y2 = (endLat - 41) * unitWidth) + 38;
+            double x1 = (startLon + 109) * unitHeight) + 38;
+            double y1 = (startLat - 41) * unitWidth) + 38;
+            double x2 = (endLon + 109) * unitHeight) + 38;
+            double y2 = (endLat - 41) * unitWidth) + 38;
             pw.println("  <line fill=\"none\" stroke=\"#0000ff\" stroke-width=\"3\" stroke-dasharray=\"null\" stroke-linejoin=\"null\" stroke-linecap=\"null\" x1=\"" + x1 + "\" y1=\"" + y1 + "\" x2=\"" + x2 + "\" y2=\"" + y2 + "\" id=\"svg_1\"/>");
         }
 
         //draw last line connected end point with start
-        endX1 = (finalEndLon + 109) * unitHeight) + 38;
-        endY1 = (finalEndLat - 41) * unitWidth) + 38;
-        endX2 = (originStartLon + 109) * unitHeight) + 38;
-        endY2 = (originStartLat - 41) * unitWidth) + 38;
+        double endX1 = (finalEndLon + 109) * unitHeight) + 38;
+        double endY1 = (finalEndLat - 41) * unitWidth) + 38;
+        double endX2 = (originStartLon + 109) * unitHeight) + 38;
+        double endY2 = (originStartLat - 41) * unitWidth) + 38;
 
         pw.println("  <line fill=\"none\" stroke=\"#0000ff\" stroke-width=\"3\" stroke-dasharray=\"null\" stroke-linejoin=\"null\" stroke-linecap=\"null\" x1=\"" + endX1 + "\" y1=\"" + endY1 + "\" x2=\"" + endX2 + "\" y2=\"" + endY2 + "\" id=\"svg_1\"/>");
 
