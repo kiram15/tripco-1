4829703d220c3a4bda20455ba1d36ebf0f76b85b
Nicole Kacirek <nicolekacirek@Nicoles-MacBook-Pro.local>
2017-10-12 14:43:56 -0600

    Adds svg to app and adds the search bar to Home

--- web/src/components/app.js
+++ web/src/components/app.js
@@ -10,30 +10,41 @@ export default class App extends React.Component {
             sysFile: [],
             total : 0,
             setInfo : [],
-            selColumns : []
-        }
-    };
-
-    render() {
-        let pairs = this.state.allPairs;
-        let ps = pairs.map((pp) => {
-            return <Pair {...pp}/>;
-        });
-        return (
-            <div className="app-container">
-                <Home
-                    browseFile={this.browseFile.bind(this)}
-                    selectColumns={this.selectColumns.bind(this)}
-                    startEndInfo={this.startEndInfo.bind(this)}
-
-                    onClick={this.onClick.bind(this)}
-                    pairs={ps}
-                    totalDist={this.state.total}
-                    columns = {this.state.setInfo}
-                />
-            </div>
-        )
+            selColumns : [],
+            //NEW:
+            serverReturned: null
     }
+};
+
+render() {
+    let pairs = this.state.allPairs;
+    let ps = pairs.map((pp) => {
+        return <Pair {...pp}/>;
+    });
+    let svg;
+       if (this.state.serverReturned) { // if this.state.serverReturned is not null
+            // set the local variable svg to this.state.serverReturned.svg
+            svg = this.state.serverReturned.svg;
+       }
+
+    return (
+
+        <div className="app-container">
+            <Home
+                browseFile={this.browseFile.bind(this)}
+                selectColumns={this.selectColumns.bind(this)}
+                startEndInfo={this.startEndInfo.bind(this)}
+
+                onClick={this.onClick.bind(this)}
+                pairs={ps}
+                totalDist={this.state.total}
+                columns = {this.state.setInfo}
+            />
+        </div>
+
+    )
+}
+
 
     onClick(val) {
         if (this.state.selColumns.indexOf(val) == -1) {
@@ -51,84 +62,77 @@ export default class App extends React.Component {
 
     }
 
-
-
-    startEndInfo(file) {
-        var finalStr = "";
-        console.log("In startEndInfo");
-        file = file.replace(/["{}]/g, "")
-        var columnNames = this.state.selColumns;
-        //var columnNames =
-        var info = file.split(',');
-        for (var i = 0; i < info.length; i++) {
+startEndInfo(file) {
+    var finalStr = "";
+    console.log("In startEndInfo");
+    file = file.replace(/["{}]/g, "")
+    var columnNames = this.state.selColumns;
+    var info = file.split(',');
+    for (var i = 0; i < info.length; i++) {
             info[i] = info[i].trim();
-        }
-        //console.log(selectedColumns);
+    }
 
-//        for (var i = 0; i < info.length; i++) {
-//            for (var j = 0; j < (selectedColumns.length); j++) {
-//                var colName = info[i].substring(0, info[i].indexOf(":"));
-//                if (colName.equals(selectedColumns[j])) {
-//                    finalStr += info[i] + "\n";
-//                }
-//            }
-//        }
-        for (var i = 0; i < info.length; i++) {
-            for (var j = 0; j < (columnNames.length); j++) {
-                var colName = info[i].substring(0, info[i].indexOf(":"));
-                if (colName ==columnNames[j]) {
-                    finalStr += info[i] + "\n";
-                }
+    for (var i = 0; i < info.length; i++) {
+        for (var j = 0; j < (columnNames.length); j++) {
+            var colName = info[i].substring(0, info[i].indexOf(":"));
+            if (colName ==columnNames[j]) {
+                finalStr += info[i] + "\n";
             }
         }
-
-        return finalStr;
     }
 
-    async selectColumns(file) {
-        //console.log("Got File:", file);
-        //console.log(file[0].startInfo);
-        var options = Object.keys(file[0].startInfo);
-        console.log("Options: ", options);
-        this.setState({
-            setInfo: options
-        });
-    }
+    return finalStr;
+}
 
-    async browseFile(file) {
-        console.log("Got file:", file);
-        //For loop that goes through all pairs,
-        let pairs = [];
-        let runTotal = 0;
-        this.selectColumns(file);
-        for (let i = 0; i < Object.values (file).length; i++) {
-            let start = file[i].start; //get start from file i
-            let end = file[i].end; //get end from file i
-            let dist = file[i].distance;
-            runTotal = runTotal + dist;
+async selectColumns(file) {
+    //console.log("Got File:", file);
+    //console.log(file[0].startInfo);
+    var options = Object.keys(file[0].startInfo);
+    console.log("Options: ", options);
+    this.setState({
+        setInfo: options
+    });
+}
 
-            var updatedStart = JSON.stringify(file[i].startInfo);
-            updatedStart = String(this.startEndInfo(updatedStart));
-            var updatedEnd = JSON.stringify(file[i].endInfo);
-            updatedEnd = String(this.startEndInfo(updatedEnd));
+async browseFile(file) {
+    console.log("Got file:", file);
+    //For loop that goes through all pairs,
+    let pairs = [];
+    let runTotal = 0;
+    this.selectColumns(file);
+    for (let i = 0; i < Object.values (file).length; i++) {
+        let start = file[i].start; //get start from file i
+        let end = file[i].end; //get end from file i
+        let dist = file[i].distance;
+        runTotal = runTotal + dist;
 
-            let p = { //create object with start, end, and dist variable
-                start: start,
-                end: end,
-                dist: dist,
-                total : runTotal,
-                startInfo : updatedStart,
-                endInfo : updatedEnd
-            };
-            pairs.push(p); //add object to pairs array
-            console.log("Pushing pair: ", p); //log to console
-        }
-        //Here we will update the state of app.
-        this.setState({
-            allPairs: pairs,
-            sysFile: file,
+        var updatedStart = JSON.stringify(file[i].startInfo);
+        updatedStart = String(this.startEndInfo(updatedStart));
+        var updatedEnd = JSON.stringify(file[i].endInfo);
+        updatedEnd = String(this.startEndInfo(updatedEnd));
+
+        let p = { //create object with start, end, and dist variable
+            start: start,
+            end: end,
+            dist: dist,
             total : runTotal,
-        });
+            startInfo : updatedStart,
+            endInfo : updatedEnd
+        };
+        pairs.push(p); //add object to pairs array
+        console.log("Pushing pair: ", p); //log to console
+    }
+    //Here we will update the state of app.
+    this.setState({
+        allPairs: pairs,
+        sysFile: file,
+        total : runTotal,
+    });
+}
+
+    // This function sends `input` the server and updates the state with whatever is returned
+    async fetch(input) {
+        console.log("Fetching...", input);
     }
 
 }
