f54869a4b855292654338b0cb8ee60627ee3f0bb
alnolte <alnolte@rams.colostate.edu>
2017-10-19 17:51:20 -0600

    absorbing front-end-search

--- web/src/components/Home/Home.jsx
+++ web/src/components/Home/Home.jsx
@@ -6,7 +6,8 @@ class Home extends React.Component {
 constructor(props) {
    super(props);
    this.state = {
-       svgImage: ''
+       svgImage : [],
+       input : []
    };
 }
 
@@ -21,7 +22,9 @@ render() {
 
 
     let total = this.props.totalDist; //update the total here
-    let displaySVG = null;
+    let svg = this.props.svg;
+    let txtSearch;
+    let displaySVG;
     if(this.state.svgImage){
         displaySVG = (<div className="svgImage"><img src={this.state.svgImage} width="70%"/></div>);
     }
@@ -33,15 +36,17 @@ render() {
             <h3>Itinerary</h3>
             <p></p>
             <div className="app-container">
-                <form id="SearchBar" onSubmit={this.props.fetch}>
-                     <input className="search-button"
-                            type="text"
-                            id="txtSearch"
-                            placeholder="What are you searching for?"
-                     />
-                     <button type="submit">Submit</button>
+
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <input size="35" className="search-button" type="text"
+                        onKeyUp={this.keyUp.bind(this)} placeholder="What are you searching for?" autoFocus/>
+                    <input type="submit" value="Submit" />
                 </form>
+
             </div>
+            <br />
+            <br />
+            <button type="button" onClick={this.buttonClicked.bind(this)}>Click here for an SVG</button>
             <p></p>
             <Dropzone className="dropzone-style" onDrop={this.drop.bind(this)}>
                 <button>Open JSON File</button>
@@ -82,7 +87,7 @@ render() {
              <Dropzone className="dropzone-style" onDrop={this.dropSVG.bind(this)}>
                 <button>Open SVG Image</button>
              </Dropzone>
-             {displaySVG}
+             {svg}
         </div>
     </div>
 }
@@ -114,7 +119,29 @@ dropSVG(acceptedFiles) {
         (file).bind(this);
         fr.readAsDataURL(file);
     });
- }
+}
+
+keyUp(event) {
+    if (event.which === 13) { // Waiting for enter to be pressed. Enter is key 13: https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+        this.props.fetch("query", this.state.input); // Call fetch and pass whatever text is in the input box
+    } else {
+        this.setState({
+            input: event.target.value
+        });
+    }
+}
+
+handleSubmit(event) {
+    this.props.fetch("query", this.state.input);
+    event.preventDefault();
+}
+
+buttonClicked(event) {
+    this.props.fetch("svg", event.target.value);
+}
+
+
+
 }
 
 export default Home
