0e657643ef86032d067ce447f846a5b794be66ca
[kira miller] <[kiram15@rams.colostate.edu]>
2017-11-04 18:28:46 -0600

    fixes some typos and added some printlines

--- src/main/java/edu/csu2017fa314/T02/Model/Hub.java
+++ src/main/java/edu/csu2017fa314/T02/Model/Hub.java
@@ -454,8 +454,112 @@ public class Hub {
     }
 
     //master method for when user selects 2opt optimization (calls all helpers)
-    public void shortestTrip3Opt(){
+    public void shortestTrip3Opt(){        //Adjacency matrix that holds all gcds
+        Object[][] gcds = calcAllGcds();
 
+        //keep track of the city that the shortest trip starts from
+        Location shortestTripStart = finalLocations.get(0);
+        //keep track of the shortest distance
+        int shortestTripDistance = 999999999;
+        //row is the current row in the adjancency matrix where the current location is
+        int row = 0;
+
+        //Create a huge distance to use for inital comparison
+        LinkedHashMap<String, String> info = new LinkedHashMap<String, String>();
+        Location bigD1 = new Location("New Zealand", -41.28650, 174.77620, info);
+        Location bigD2 = new Location("Madrid", 40.41680, -3.70380, info);
+        Distance hugeDistance = new Distance(bigD1, bigD2, miles);
+
+        //temp array list to keep track of the cities we have been to
+        ArrayList<Location> traveledTo = new ArrayList<Location>();
+
+        //for each location in the finalLocations array list: picking a starting city
+        for (Location l : finalLocations) {
+            //set the first city in the finalLocations array list to our current location
+            Location currentLocation = l;
+            int tripDistance = 0;
+
+            //while there are still more cities to travel to
+            while (traveledTo.size() < finalLocations.size()) {
+                for (int i = 0; i < finalLocations.size(); i++) {
+                    if (finalLocations.get(i).equals(currentLocation)) {
+                        row = i;
+                    }
+                }
+                traveledTo.add(currentLocation);
+                if (traveledTo.size() == finalLocations.size()) {
+                    break;
+                }
+                Distance shortestDistance = hugeDistance;
+                for (int i = 1; i < gcds[0].length; i++) { //because we aren't including initial location
+                    Distance d = (Distance) gcds[row][i];
+                    if (!traveledTo.contains(d.getEndID()) && (d.getGcd() < shortestDistance.getGcd())) {
+                        shortestDistance = d;
+                    }
+                }
+                currentLocation = shortestDistance.getEndID();
+            }
+
+            //add the distance back to the original cit
+            Object[] backAround = gcds[row];
+            //grab the distance from the current city to original city
+            Distance temp = new Distance(currentLocation, l, miles);
+            for (int i = 1; i < backAround.length; i++) {
+                Distance d = (Distance) backAround[i];
+                //add to tripDistance
+                if (temp.equals(d)) {
+                    tripDistance += d.getGcd();
+                }
+            }
+
+            //apply 2opt
+            checkImprovement3(traveledTo);
+
+            //get the updated trip distance after 2opt
+            ArrayList<Distance> traveledDistances = locationsToDistances(traveledTo);
+            for (int i = 0; i < traveledDistances.size(); i++) {
+                tripDistance += traveledDistances.get(i).getGcd();
+            }
+
+            //making traveledTo empty again
+            traveledTo = new ArrayList<Location>();
+
+            //compare the final distance to the stored shortest distance
+            if (tripDistance < shortestTripDistance) {
+                //if the trip was shorter then store distance and starting city
+                shortestTripDistance = tripDistance;
+                shortestTripStart = l;
+            }
+        }
+
+        //start final trip at the predetermined shortest trip start
+        Location currentLocation = shortestTripStart;
+
+        ArrayList<Location> traveledToFinal = new ArrayList<Location>();
+        //while there are still more cities to travel to
+        while (traveledToFinal.size() < finalLocations.size()) {
+            for (int i = 0; i < finalLocations.size(); i++) {
+                if (finalLocations.get(i).equals(currentLocation)) {
+                    row = i;
+                }
+            }
+            traveledToFinal.add(currentLocation);
+            if (traveledToFinal.size() == finalLocations.size()) {
+                break;
+            }
+            Distance shortestDistance = hugeDistance;
+            for (int i = 1; i < gcds[0].length; i++) { //because we aren't including first Location
+                Distance d = (Distance) gcds[row][i];
+                if (!traveledToFinal.contains(d.getEndID()) && (d.getGcd() < shortestDistance.getGcd())) {
+                    shortestDistance = d;
+                }
+            }
+            currentLocation = shortestDistance.getEndID();
+        }
+        //apply 2opt
+        checkImprovement3(traveledToFinal);
+        //convert traveledToFinal location array to a distance array
+        shortestItinerary = locationsToDistances(traveledToFinal);
     }
 
     //will return an array list with each city listed once, with the shortest city as its end
@@ -512,9 +616,9 @@ public class Hub {
                         Distance ij = new Distance(traveled.get(i), traveled.get(j), miles);
                         Distance ij1 = new Distance(traveled.get(i), traveled.get(j+1), miles);
                         Distance ik = new Distance(traveled.get(i), traveled.get(k), miles);
-                        Distance ik1 = new Distance(traveled.get(i), traveled.get(k+1), miles);
+                        //Distance ik1 = new Distance(traveled.get(i), traveled.get(k+1), miles);
 
-                        Distance i1j = new Distance(traveled.get(i+1), traveled.get(j), miles);
+                        //Distance i1j = new Distance(traveled.get(i+1), traveled.get(j), miles);
                         Distance i1j1 = new Distance(traveled.get(i+1), traveled.get(j+1), miles);
                         Distance i1k = new Distance(traveled.get(i+1), traveled.get(k), miles);
                         Distance i1k1 = new Distance(traveled.get(i+1), traveled.get(k+1), miles);
@@ -523,7 +627,7 @@ public class Hub {
                         Distance jk = new Distance(traveled.get(j), traveled.get(k), miles);
                         Distance jk1 = new Distance(traveled.get(j), traveled.get(k+1), miles);
 
-                        Distance j1k = new Distance(traveled.get(j+1), traveled.get(k), miles);
+                        //Distance j1k = new Distance(traveled.get(j+1), traveled.get(k), miles);
                         Distance j1k1 = new Distance(traveled.get(j+1), traveled.get(k+1), miles);
 
                         Distance kk1 = new Distance(traveled.get(k), traveled.get(k+1), miles);
