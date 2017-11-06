2f1ac1201131f959ee447d7160be1cfc0ae2c3e3
Nicole Kacirek <nicolekacirek@Nicoles-MacBook-Pro.local>
2017-11-06 13:23:55 -0700

    add console log to checked selectedLocations[i]

--- web/src/components/Home/Home.jsx
+++ web/src/components/Home/Home.jsx
@@ -41,20 +41,6 @@ render() {
     }
     console.log("MyDiv", myDiv);
 
-    var selected = document.getElementById("checkedLocations");
-    for (var i = 0; i < (this.state.selectedLocations.length); i++) {
-        var inputSL = document.createElement("input");
-        var labelSL = document.createElement("label");
-        var br = document.createElement("br");
-        inputSL.type = "text";
-        inputSL.value = this.state.selectedLocations[i];
-        selected.appendChild(inputSL);
-        selected.appendChild(labelSL);
-        labelSL.appendChild(document.createTextNode(this.state.selectedLocations[i]));
-        selected.appendChild(br);
-    }
-    console.log("selected Div", selected);
-
     let total = this.props.totalDist; //update the total here
     let svg = this.props.svg;
     let txtSearch;
@@ -235,6 +221,20 @@ updateSelectedLocations(event) {
         }
     }
     console.log("selectedLocations:", this.state.selectedLocations);
+    var selected = document.getElementById("checkedLocations");
+        for (var i = 0; i < (this.state.selectedLocations.length); i++) {
+            var inputSL = document.createElement("input");
+            var labelSL = document.createElement("label");
+            var brSL = document.createElement("br");
+            inputSL.type = "text";
+            inputSL.value = this.state.selectedLocations[i];
+            console.log("selected Div", selected);
+            selected.appendChild(inputSL);
+            selected.appendChild(labelSL);
+            labelSL.appendChild(document.createTextNode(this.state.selectedLocations[i]));
+            selected.appendChild(brSL);
+        }
+        console.log("selected Div", selected);
 }
 
 selectAll(source) {
