e55761f71eddd4600daae64e3ce968773d0b0e02
Emerson McIntosh <emerson.mcintosh@gmail.com>
2017-12-04 10:31:05 -0700

    maybe this does something

--- src/main/java/edu/csu2017fa314/T02/Model/Hub.java
+++ src/main/java/edu/csu2017fa314/T02/Model/Hub.java
@@ -199,7 +199,9 @@ public class Hub {
         }
     }
 
-    //creates itinerary list order based on the optimization global
+    /*
+    * creates itinerary list order based on the optimization global
+    */
     public void createItinerary(){
         //switch statement that calls the specific shortest trip method based on selected optimization
         switch(optimization){
@@ -225,7 +227,9 @@ public class Hub {
         }
     }
 
-    //stores the info about each airport in the location lists
+    /*
+    *stores the info about each airport in the location lists
+    */
     public void storeColumnHeaders(String firstLine){
         String s = firstLine.toLowerCase();
         String[] infoArray = s.split(",");
@@ -252,7 +256,9 @@ public class Hub {
         }
     }
 
-    //splits up rows to add the
+    /*
+    * splits up rows to add to the selected locations
+    */
     public void parseRow(String row){
         row = row.toLowerCase();
         String[] props = row.split(",");
@@ -283,7 +289,9 @@ public class Hub {
         selectedLocations.add(location);
     }
 
-    //conerts string latitude or longitude (s) to double
+    /*
+    *converts string latitude or longitude (s) to double
+    */
     public double latLonConvert(String s) {
         String sCopy = s;
         int end;
@@ -337,7 +345,9 @@ public class Hub {
         }
     }
 
-    //preforms the swap method for 2opt and 3opt
+    /*
+    * preforms the swap method for 2opt and 3opt
+    */
     public void optSwap(ArrayList<Location> traveledTo, int i1, int k) { // swap in place
         while (i1 < k) {
             // reverses all the elements from i+1 to k
@@ -351,7 +361,9 @@ public class Hub {
         }
     }
 
-    //will return an array list with each city listed once, with the shortest city as its end
+    /*
+    *will return an array list with each city listed once, with the shortest city as its end
+    */
     public Object[][] calcAllGcds(ArrayList<Location> selectedLocations) {
         Object[][] GCDS = new Object[selectedLocations.size()][selectedLocations.size()+1];
         for (int i = 0; i < selectedLocations.size(); i++) {
@@ -368,8 +380,9 @@ public class Hub {
         return GCDS;
     }
 
-    //transforms an arrayList of location objects into an arrayList of distance objects using the
-    //location objects in the order they are passed in
+    /*transforms an arrayList of location objects into an arrayList of distance objects using the
+    * location objects in the order they are passed in
+    */
     public ArrayList<Distance> locationsToDistances(ArrayList<Location> locations) {
         ArrayList<Distance> finalDistances = new ArrayList<Distance>();
         for (int i = 0; i < locations.size(); i++) {
