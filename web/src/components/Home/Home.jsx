69f0222e455691f3fc4da5230dfca90d7a7225d2
Nicole Kacirek <nicolekacirek@Nicoles-MacBook-Pro.local>
2017-12-06 19:41:05 -0700

    adds Remove Button

--- web/src/components/Home/Home.jsx
+++ web/src/components/Home/Home.jsx
@@ -89,7 +89,8 @@ render() {
 
   <button type="button" onClick={this.updateSelectedLocations.bind(this)}>Select</button>
   <button type="button" onClick={this.selectAll.bind(this)}>Select All</button>
-  <button type="button" onClick={this.clearAll.bind(this)}>Clear All</button>
+    <button type="button" onClick={this.clearAll.bind(this)}>Remove</button>
+  <button type="button" onClick={this.clearAll.bind(this)}>Remove All</button>
   <p></p>
 
   <p className="w3-myFont">Selected Locations</p>
